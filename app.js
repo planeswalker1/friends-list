2715df8e515cd847c48c3a61ab963e6b4ea2ddcb
Daniel Munoz <daniel.munozdev@gmail.com>
2018-09-02 15:43:32 -0700

    add form, and post route to update friend list

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
 let friends = ['Brian', 'David', 'Jorde', 'Raf', 'Taka'];
 
 app.get('/', function (req, res) {
   res.render('index', {friends: friends});
+});
+
+app.post('/addfriend', function (req, res) {
+  let newFriend = req.body.friendName;
+  friends.push(newFriend);
+  res.redirect('/');
 });
 
 app.listen(PORT, function () {
